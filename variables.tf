4064f4249c1562b61ea16090bd26b382fdb96f29
Birendra Behera <birendra036@gmail.com>
2023-05-21 13:37:37 +0530

    added EKS terraform code

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "location" {
+    default = "us-east-1"
+}
+
+variable "os_name" {
+    default = "ami-053b0d53c279acc90"
+}
+
+variable "key" {
+    default = "devs-key"
+}
+
+variable "instance-type" {
+    default = "t2.small"
+}
+
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+}
+
+variable "subnet2-cidr" {
+    default = "10.10.2.0/24"
+}
+
+variable "subent1_az" {
+    default =  "us-east-1a"  
+}
+
+variable "subent2_az" {
+    default =  "us-east-1b"  
+}
